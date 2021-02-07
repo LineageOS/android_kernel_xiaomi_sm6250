dc44ed72c12107df8b927e3935a061fa59d5ac9c
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-07 17:58:04 +0300

    drivers: input: fingerprint: nuke GF_KEY_HOME key reporting
    * can cause weirdness with quickswitch
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: Icdb60f421fd33e7973671aef2bca0c56e6dec12e
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/input/fingerprint/gf3208/gf_spi.c
+++ drivers/input/fingerprint/gf3208/gf_spi.c
@@ -411,11 +411,6 @@ static void gf_kernel_key_input(struct gf_dev *gf_dev, struct gf_key *gf_key)
 		input_report_key(gf_dev->input, key_input, 0);
 		input_sync(gf_dev->input);
 	}
-
-	if (gf_key->key == GF_KEY_HOME) {
-		input_report_key(gf_dev->input, key_input, gf_key->value);
-		input_sync(gf_dev->input);
-	}
 }
 
 static long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
